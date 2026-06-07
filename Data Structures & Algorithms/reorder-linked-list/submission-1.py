e8185a574d06c24e0548319bbb92771b053a08d2
reddyrahul002 <reddyrahulyerram@gmail.com>
2026-06-07 12:46:17 +1000

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.py
@@ -0,0 +1,96 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # Task1
+        fast=head
+        slow=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        second=slow.next
+        slow.next=None
+        
+        # Task2
+        prev=None
+        while second:
+            next=second.next
+            second.next=prev
+            prev=second
+            second=next
+
+        # Task3
+        first=head
+        second=prev
+        while first and second:
+            temp1=first.next
+            temp2=second.next
+
+            first.next=second
+            second.next=temp1
+
+            first=temp1
+            second=temp2
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # fast=head
+        # slow=head
+        # while fast and fast.next:
+        #     slow=slow.next
+        #     fast=fast.next.next
+        
+        # second = slow.next
+        # slow.next=None
+        
+        # prev=None
+        # current =second
+        # while current:
+        #     next = current.next
+        #     current.next=prev
+        #     prev=current
+        #     current=next
+        
+        # second = prev
+        # first = head
+
+        # while first and second:
+        #     temp1=first.next
+        #     temp2=second.next
+
+        #     first.next=second
+        #     second.next=temp1
+            
+        #     first=temp1
+        #     second=temp2
+        
+        # # return head
+
+        
+
+            
+ 
+
+        
