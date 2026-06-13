8ea9c919ac64cdff508345c3cca97bfb83b10c8d
reddyrahul002 <reddyrahulyerram@gmail.com>
2026-06-13 10:18:23 +1000

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        store={1:1,2:2}
+        
+        def f(x):
+            if x in store:
+                return store[x]
+            else:
+                store[x]=f(x-1)+f(x-2)
+            return store[x]
+        
+        return f(n)
+    
