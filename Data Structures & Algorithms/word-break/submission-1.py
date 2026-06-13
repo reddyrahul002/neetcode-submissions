d4d54daa2054c2f7bf2bc2f92893834fc62e9e46
reddyrahul002 <reddyrahulyerram@gmail.com>
2026-06-13 18:19:28 +1000

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordset=set(wordDict)
+        memo={}
+        def canbreak(start):
+            if start==len(s):
+                return True
+            if start in memo:
+                return memo[start]
+            for i in range(start+1,len(s)+1):
+                word=s[start:i]
+                if word in wordset and canbreak(i):
+                    memo[start]=True
+                    return True
+            memo[start]=False
+            return False
+        
+        return canbreak(0)
+        
+            
+
+        
