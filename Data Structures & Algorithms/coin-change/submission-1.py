48f1c85711d2f738f940b95a79bbdd5d06855712
reddyrahul002 <reddyrahulyerram@gmail.com>
2026-06-13 15:50:50 +1000

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n=len(coins)
+        coins.sort()
+        memo={0:0}
+        def min_coins(amt):
+            if amt in memo:
+                return memo[amt]
+            minn=float('inf')
+            for coin in coins:
+                diff= amt-coin
+                if diff<0:
+                    break
+                minn=min(minn,1+min_coins(diff))
+            memo[amt]=minn
+            return minn
+        result=min_coins(amount)
+        if result <float('inf'):
+            return result
+        else:
+            return -1
