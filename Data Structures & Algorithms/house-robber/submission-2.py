9c3024846d30bc07e6bcced4b529d9aa565242ab
reddyrahul002 <reddyrahulyerram@gmail.com>
2026-06-13 13:19:01 +1000

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        store={}
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        store={0:nums[0],1:max(nums[0],nums[1])}
+
+        def mxM(x):
+            if x in store:
+                return store[x]
+            store[x] = max(nums[x]+mxM(x-2),mxM(x-1))
+            return store[x]
+        
+        return mxM(len(nums)-1)
+        
