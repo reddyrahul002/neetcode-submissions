d946dd40442b3d63b544859862a54e85cab304bc
reddyrahul002 <reddyrahulyerram@gmail.com>
2026-06-14 21:45:30 +1000

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last ={c:i for i,c in enumerate(s)}
+        result=[]
+
+        start=0
+        end=0
+        for i,c in enumerate(s):
+            end = max(end,last[c])
+            if i==end:
+                result.append(end-start+1)
+                start=i+1
+        return result
+            
+        
