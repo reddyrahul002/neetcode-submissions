3b9fd61e2db62c72318f55a72acaf7dabdc70d42
reddyrahul002 <reddyrahulyerram@gmail.com>
2026-06-24 13:02:31 +1000

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,29 @@
+class WordDictionary:
+    def __init__(self):
+        self.is_end=False
+        self.children={}
+    def addWord(self, word: str) -> None:
+        node=self
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch]= WordDictionary()
+            node = node.children[ch]
+        node.is_end = True
+    def search(self, word: str) -> bool:
+        node=self
+        for i,ch in enumerate(word):
+            if ch=='.':
+                for child in node.children.values():
+                    if child.search(word[i+1:]):
+                        return True
+                return False
+            else:
+                if ch not in node.children:
+                    return False
+                node=node.children[ch]
+        return node.is_end
+
+
+
+
+        
